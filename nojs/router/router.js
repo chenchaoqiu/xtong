c55039173309c8f332a009a03c197d94703dc7f0
陈林 <2380695900@qq.com>
2017-07-18 11:46:15 +0800

    优化路由，添加操作文件

---
+++ nojs/router/router.js
@@ -0,0 +1,5 @@
+﻿var router = require('../router.js');
+module.exports = function (app) {
+    /*生成路由*/
+    router(app,'/','./view/index.html');
+};
--- nojs/server.js
+++ nojs/server.js
@@ -1,15 +1,22 @@
 ﻿var express = require('express');
 var app = express();
 var fs = require('fs'); /*文件上传这个也要*/
+
 /*文件上传multer*/
 var multer = require('multer');
+
 /*这个body-parser是post提交的前提*/
-var bodyParser = require('body-parser');//
-// /*配置路由*/
-var router = require('./router.js');
+var bodyParser = require('body-parser');
 
 // 创建 application/x-www-form-urlencoded 编码解析   post
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/*引入cookie*/
+var cookieParser = require('cookie-parser');
+
+// /*配置路由*/
+var router = require('./router/router.js')(app);
+
 
 /*打开静态文件夹或文件*/
 app.use(express.static('view/public'));
@@ -17,10 +24,6 @@ app.use(express.static('view/public'));
 /*上传文件*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ dest: '/tmp/'}).array('image'));
-
-// /*配置路由*/
-new router(app,'/','view/index.html');
-// new router(app,'/jquery-2.0.0.min.js','view/js/jquery-2.0.0.min.js');
 
 
 app.get('/process_get', function (req, res) {
@@ -32,7 +35,54 @@ app.get('/process_get', function (req, res) {
     };
     console.log(response);
     res.end(JSON.stringify(response));
+});
+
+/*读取数据*/
+app.get('/listuser',function (req, res) {
+    fs.readFile(__dirname + '/' + 'json/users.json','utf8',function (err, data) {
+        console.log(data);
+        res.end(JSON.stringify(data));
+    })
 })
+/*添加数据*/
+var user = {
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    }
+};
+app.get('/addUser', function (req, res) {
+    // 读取已存在的数据
+    fs.readFile( __dirname + "/" + "json/users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+});
+
+/*显示用户详情*/
+app.get('/cksl/:id', function (req, res) {
+    // 首先我们读取已存在的用户
+    fs.readFile( __dirname + "/" + "json/users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        var user = data["user" + req.params.id]
+        console.log( user );
+        res.end( JSON.stringify(user));
+    });
+});
+
+/*删除用户*/
+app.get('/deleteUser/:id', function (req, res) {
+    fs.readFile( __dirname + "/" + "json/users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        delete data["user" + req.params.id];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+});
 
 app.post('/process_post', urlencodedParser, function (req, res) {
     /*post 方式输出输入*/
@@ -65,6 +115,14 @@ app.post('/file_upload', function (req, res) {
         });
     });
 })
+
+/*cookies 管理*/
+app.use(cookieParser());
+app.get('/COO', function(req, res) {
+    req.cookies.sle='lsjei';
+    console.log("Cookies: ", req.cookies);
+})
+
 
 var server = app.listen(8081, function () {
 /*配置服务器*/

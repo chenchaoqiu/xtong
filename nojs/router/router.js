0741f00186cdd5bf59c81988c61722e072b79c74
陈林 <2380695900@qq.com>
2017-08-14 12:04:45 +0800

    加入答题游戏

--- nojs/router/router.js
+++ nojs/router/router.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
     /*生成路由*/
     router(app,'/','./view/index.html');
     router(app,'/index.html','./view/index.html');
-
     router(app,'/file','./view/file.html');
+    router(app,'/answer','./view/Answer.html');
+    router(app,'/share','./view/richScan.html');
 };
